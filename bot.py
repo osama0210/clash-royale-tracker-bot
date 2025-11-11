86b478406a69251e8e60b9d78dde1c9ef7b25d69
aatje-sy <ahas.alasmi@gmail.com>
2025-11-11 11:18:33 +0100

    bot.py fixed

--- bot.py
+++ bot.py
@@ -1,24 +1,24 @@
-import os
+import os, asyncio
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()  # laad .env bestand
-
-TOKEN = os.getenv("DISCORD_TOKEN")
-CLASH_API_KEY = os.getenv("CLASH_API_KEY")
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-print("Token:", os.getenv("DISCORD_TOKEN"))
+
 @bot.event
 async def on_ready():
-    print(f" Ingelogd als {bot.user}")
+    print(f"✅ Ingelogd als {bot.user}")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong!")
+async def main():
+    async with bot:
+        await bot.load_extension("commands.player")
 
-bot.run(TOKEN)
+        await bot.start(os.getenv("DISCORD_TOKEN"))
+
+if __name__ == "__main__":
+    asyncio.run(main())
